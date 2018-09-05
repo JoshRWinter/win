8d29fa82315d7c0565b0cdd4134ddab81600d671
Josh Winter <winterjoshr@gmail.com>
2018-09-04 21:24:32 -0400

    roll tool supports a "rollfile" in the current directory

--- tools/roll.cpp
+++ tools/roll.cpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include <vector>
 #include <memory>
+#include <cctype>
 
 #include <string.h>
 #include <stdio.h>
@@ -16,26 +17,32 @@ using namespace std::string_literals;
 
 static constexpr const char *const helptext =
 "Asset Roll\n"
-"Usage: roll output-file inputfile[:z] [ ... ]"
+"Usage: roll output-file.roll inputfile[:z] [ ... ]\n"
+"       roll output-file.roll\n"
+"\n"
+"If the second 1-arg form is used, a 'rollfile' is\n"
+"expected to be found in the current direcory.\n"
+"\n"
+"The rollfile should have one file (with optional\n"
+"[:z] compression flag) per line. #Comments, blank\n"
+"lines, and end-of-line #Comments are supported here."
 ;
+
 static const std::string magic = "ASSETROLL";
 
 static std::string strip_options(const std::string&);
+static std::string forward_slash(const std::string&);
+static std::string trim(const std::string&);
 static bool request_compression(const std::string&);
 static bool is_asset_roll(const std::string&);
 static long long filesize(const std::string&);
 static bool exists(const std::string&);
 static std::string format(size_t);
-static int go(int, char**);
+static void create(const std::string&, const std::vector<std::string>&);
+static int go(int argc, char **argv);
 
 int main(int argc, char **argv)
 {
-	if(argc < 3)
-	{
-		std::cout << helptext << std::endl;
-		return 0;
-	}
-
 	try
 	{
 		return go(argc, argv);
@@ -47,22 +54,73 @@ int main(int argc, char **argv)
 	}
 }
 
-// do the work
 int go(int argc, char **argv)
 {
+	if(argc < 2)
+	{
+		std::cout << helptext << std::endl;
+		return 0;
+	}
+
+	// collect the arguments
+	std::vector<std::string> infiles;
+
+	if(argc == 2)
+	{
+		// use rollfile
+		std::ifstream rollfile("rollfile");
+		if(!rollfile)
+			throw std::runtime_error("No rollfile could be found in the current directory");
+
+		while(rollfile.good())
+		{
+			std::string line;
+			std::getline(rollfile, line);
+
+			line = forward_slash(trim(line));
+
+			// ignore blanks
+			if(line.length() == 0)
+				continue;
+
+			// ignore lines starting with #
+			if(line[0] == '#')
+				continue;
+
+			// cut the comment end off a line (e.g. "filepath.txt #comment")
+			const auto position = line.find("#");
+			if(position != std::string::npos)
+				line = line.substr(0, position);
+
+			infiles.push_back(line);
+		}
+	}
+	else
+	{
+		for(int i = 2; i < argc; ++i)
+			infiles.push_back(trim(forward_slash(argv[i])));
+	}
+
+	create(argv[1], infiles);
+
+	return 0;
+}
+
+// do the work
+void create(const std::string &out_file, const std::vector<std::string> &in_files)
+{
 	// number of input files
-	const int file_count = argc - 2;
+	const int file_count = in_files.size();
 
 	// make sure the output file doesn't exist, or is already an asset roll
-	if(exists(argv[1]) && !is_asset_roll(argv[1]))
-		throw std::runtime_error("Not willing to overwrite file \""s + argv[1] + "\"");
+	if(exists(out_file) && !is_asset_roll(out_file))
+		throw std::runtime_error("Not willing to overwrite file \""s + out_file + "\"");
 
 	// make sure all the input files exist
-	for(int i = 2; i < argc; ++i)
+	for(const std::string &file : in_files)
 	{
-		const std::string stripped = strip_options(argv[i]);
-		if(!exists(stripped))
-			throw std::runtime_error("File \"" + stripped + "\" does not exist");
+		if(!exists(strip_options(file)))
+			throw std::runtime_error("File \"" + strip_options(file) + "\" does not exist");
 	}
 
 	// construct the headers
@@ -88,14 +146,14 @@ int go(int argc, char **argv)
 		h.uncompressed_size;
 		h.begin = 0;
 		h.size = 0;
-		h.filename = strip_options(argv[i + 2]);
+		h.filename = strip_options(in_files[i]);
 		h.filename_length = h.filename.length();
 
 		headers.push_back(h);
 	}
 
 	// prepare output file
-	std::ofstream out(argv[1], std::ofstream::binary);
+	std::ofstream out(out_file, std::ofstream::binary);
 
 	// write magic
 	out.write(magic.c_str(), magic.length());
@@ -124,7 +182,7 @@ int go(int argc, char **argv)
 		const long long fsize = filesize(headers[i].filename);
 
 		// fill in some missing header details
-		headers[i].compressed = request_compression(argv[i + 2]);
+		headers[i].compressed = request_compression(in_files[i]);
 		headers[i].uncompressed_size = fsize;
 		headers[i].begin = offset;
 		if(!headers[i].compressed)
@@ -192,9 +250,7 @@ int go(int argc, char **argv)
 
 	out.close();
 
-	std::cout << file_count << " files written to \"" << argv[1] << "\" (" << format(filesize(argv[1])) << ")" << std::endl;
-
-	return 0;
+	std::cout << file_count << " files written to \"" << out_file << "\" (" << format(filesize(out_file)) << ")" << std::endl;
 }
 
 std::string format(size_t size)
@@ -218,6 +274,48 @@ std::string strip_options(const std::string &filename)
 		return filename;
 	else
 		return filename.substr(0, filename.length() - 2);
+}
+
+// replace backslashes with forward slashes
+std::string forward_slash(const std::string &name)
+{
+	if(name.find('\\') == std::string::npos)
+		return name;
+
+	std::string newname = name;
+	for(char &c : newname)
+		if(c == '\\')
+			c = '/';
+
+	return newname;
+}
+
+// remove whitespace on both sides
+std::string trim(const std::string &s)
+{
+	std::string str = s;
+
+	// trim beginning
+	for(int i = 0; i < str.size(); ++i)
+	{
+		if(isspace(str[i]))
+		{
+			str.erase(str.begin() + i);
+			--i;
+		}
+	}
+
+	// trim end
+	for(int i = str.size() - 1; i >= 0; --i)
+	{
+		if(isspace(str[i]))
+		{
+			str.erase(str.begin() + i);
+			++i;
+		}
+	}
+
+	return str;
 }
 
 bool request_compression(const std::string &filename)
