e2a43a4ce37bec628435da85ac915092fc87b2cf
Josh Winter <winterjoshr@gmail.com>
2019-08-27 15:49:40 -0400

    align and angle_diff functions

--- win/src/utility.h
+++ win/src/utility.h
@@ -200,6 +200,8 @@ const char *key_name(button);
 
 // random useful utilities
 float distance(float, float, float, float);
+float align(float, float, float);
+float angle_diff(float, float);
 float zerof(float, float);
 
 }
