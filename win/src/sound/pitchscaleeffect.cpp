b93607b89a4842144e5c180947bcdd74f989e73c
Josh Winter <winterjoshr@gmail.com>
2022-11-20 16:59:54 -0500

    remove pitchscaleeffect implementation

--- win/src/sound/pitchscaleeffect.cpp
+++ win/src/sound/pitchscaleeffect.cpp
@@ -10,18 +10,7 @@ PitchScaleEffect::PitchScaleEffect(int priority, float scale)
 
 int PitchScaleEffect::read_samples(float *const dest, const int len)
 {
-	if (inner == NULL)
-		win::bug("SoundEffect: null inner");
-	if (len / 2 > default_working_buffer_size)
-		win::bug("PitchScaleEffect: request len too big");
-
-	float convbuf[default_working_buffer_size];
-	const int got = inner->read_samples(convbuf, len / 2);
-
-	for (int i = 0; i < len; ++i)
-		dest[i] = convbuf[i / 2];
-
-	return got * 2;
+	win::bug("this is unimplemented");
 }
 
 void PitchScaleEffect::set_scale(float s)
