422ed0e7651483434e7b2b5619145c1f1ac65168
Josh Winter <winterjoshr@gmail.com>
2023-04-18 23:51:26 -0400

    contiguous reservations

--- tests/MappedRingBufferTests.cpp
+++ tests/MappedRingBufferTests.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 
 #define private public
+#define protected public
 #include <win/MappedRingBuffer.hpp>
 
 int successfull = 0;
@@ -14,7 +15,7 @@ template <typename T> std::vector<win::MappedRingBufferRangeSlice> vec(const win
 	return v;
 }
 
-template <typename T> void test_mapped_buffer()
+template <typename T> void test_mapped_buffer_reserve()
 {
 	T mapped[5];
 	win::MappedRingBuffer<T> mbuf(mapped, 5);
@@ -196,7 +197,7 @@ template <typename T> void test_mapped_buffer_range_write()
 	}
 }
 
-template <typename T> void test_iterator()
+template <typename T> void test_mapped_buffer_range_iterator()
 {
 	T mapped[5] = { 0, 0, 0, 0, 0 };
 	win::MappedRingBuffer<T> mbuf(mapped, 5);
@@ -342,7 +343,7 @@ template <typename T> void test_iterator()
 	}
 }
 
-template <typename T> void test_subscript_operator()
+template <typename T> void test_mapped_buffer_range_subscript_operator()
 {
 	T mapped[5] = { 0, 0, 0, 0, 0 };
 	win::MappedRingBuffer<T> mbuf(mapped, 5);
@@ -443,27 +444,419 @@ template <typename T> void test_subscript_operator()
 	}
 }
 
+template <typename T> void test_mapped_buffer_contiguous_reserve()
+{
+	T mapped[5];
+	win::MappedRingBuffer<T> mbuf(mapped, 5);
+
+	assert(mbuf.buffer == mapped);
+	assert(mbuf.buffer_length == 5);
+
+	{
+		auto range = mbuf.reserve_contiguous(5);
+
+		assert(mbuf.buffer_head == 0);
+		assert(range.range_head == 0);
+		assert(range.range_length == 5);
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(1);
+
+		assert(mbuf.buffer_head == 1);
+		assert(range.range_head == 0);
+		assert(range.range_length == 1);
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(3);
+
+		assert(mbuf.buffer_head == 4);
+		assert(range.range_head == 1);
+		assert(range.range_length == 3);
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(1);
+
+		assert(mbuf.buffer_head == 0);
+		assert(range.range_head == 4);
+		assert(range.range_length == 1);
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(4);
+
+		assert(mbuf.buffer_head == 4);
+		assert(range.range_head == 0);
+		assert(range.range_length == 4);
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(2);
+
+		assert(mbuf.buffer_head == 2);
+		assert(range.range_head == 0);
+		assert(range.range_length == 2);
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(1);
+
+		assert(mbuf.buffer_head == 3);
+		assert(range.range_head == 2);
+		assert(range.range_length == 1);
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(4);
+
+		assert(mbuf.buffer_head == 4);
+		assert(range.range_head == 0);
+		assert(range.range_length == 4);
+	}
+}
+
+template <typename T> void test_mapped_buffer_range_contiguous_write()
+{
+	T mapped[5] { 0, 0, 0, 0, 0 };
+	win::MappedRingBuffer<T> mbuf(mapped, 5);
+
+	assert(mbuf.buffer == mapped);
+	assert(mbuf.buffer_length == 5);
+
+	{
+		auto range = mbuf.reserve_contiguous(3);
+
+		assert(mbuf.buffer_head == 3);
+		assert(range.range_head == 0);
+		assert(range.range_length == 3);
+
+		{
+			const T src[] = { 39, 45 };
+			const int wrote = range.write(src, 2);
+
+			assert(wrote == 2);
+			assert(mapped[0] == 39 && mapped[1] == 45 && mapped[2] == 0 && mapped[3] == 0 && mapped[4] == 0);
+		}
+
+		{
+			const T src[] = { 99, 77 };
+			const int wrote = range.write(1, src, 2);
+
+			assert(wrote == 2);
+			assert(mapped[0] == 39 && mapped[1] == 99 && mapped[2] == 77 && mapped[3] == 0 && mapped[4] == 0);
+		}
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(3);
+
+		assert(mbuf.buffer_head == 3);
+		assert(range.range_head == 0);
+		assert(range.range_length == 3);
+
+		{
+			const T src[] = { 69, 5 };
+			const int wrote = range.write(src, 2);
+
+			assert(wrote == 2);
+			assert(mapped[0] == 69 && mapped[1] == 5 && mapped[2] == 77 && mapped[3] == 0 && mapped[4] == 0);
+		}
+
+		{
+			const T src[] = { 40 };
+			const int wrote = range.write(2, src, 1);
+
+			assert(wrote == 1);
+			assert(mapped[0] == 69 && mapped[1] == 5 && mapped[2] == 40 && mapped[3] == 0 && mapped[4] == 0);
+		}
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(1);
+
+		assert(mbuf.buffer_head == 4);
+		assert(range.range_head == 3);
+		assert(range.range_length == 1);
+
+		{
+			const T src[] = { 3 };
+			const int wrote = range.write(src, 1);
+
+			assert(wrote == 1);
+			assert(mapped[0] == 69 && mapped[1] == 5 && mapped[2] == 40 && mapped[3] == 3 && mapped[4] == 0);
+		}
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(2);
+
+		assert(mbuf.buffer_head == 2);
+		assert(range.range_head == 0);
+		assert(range.range_length == 2);
+
+		{
+			const T src[] = { 55, 39  };
+			const int wrote = range.write(src, 2);
+
+			assert(wrote == 2);
+			assert(mapped[0] == 55 && mapped[1] == 39 && mapped[2] == 40 && mapped[3] == 3 && mapped[4] == 0);
+		}
+	}
+}
+
+template <typename T> void test_mapped_buffer_contiguous_range_iterator()
+{
+	T mapped[5] = { 0, 0, 0, 0, 0 };
+	win::MappedRingBuffer<T> mbuf(mapped, 5);
+
+	assert(mbuf.buffer == mapped);
+	assert(mbuf.buffer_length == 5);
+	assert(mbuf.buffer_head == 0);
+
+	{
+		auto range = mbuf.reserve_contiguous(2);
+
+		assert(mbuf.buffer_head == 2);
+		assert(range.range_head == 0);
+		assert(range.range_length == 2);
+
+		int index = 0;
+		for (auto &item : range)
+		{
+			item = index * 3;
+			++index;
+		}
+
+		assert(mapped[0] == 0 && mapped[1] == 3 && mapped[2] == 0 && mapped[3] == 0 && mapped[4] == 0);
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(4);
+
+		assert(mbuf.buffer_head == 4);
+		assert(range.range_head == 0);
+		assert(range.range_length == 4);
+
+		int index = 4;
+		for (auto &item : range)
+		{
+			item = index * 4;
+			++index;
+		}
+
+		assert(mapped[0] == 16 && mapped[1] == 20 && mapped[2] == 24 && mapped[3] == 28 && mapped[4] == 0);
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(3);
+
+		assert(mbuf.buffer_head == 3);
+		assert(range.range_head == 0);
+		assert(range.range_length == 3);
+
+		{
+			auto it = range.begin();
+
+			assert(it.position == 0);
+
+			*it = 5;
+			++it;
+
+			assert(it.position == 1);
+			assert(mapped[0] == 5 && mapped[1] == 20 && mapped[2] == 24 && mapped[3] == 28 && mapped[4] == 0);
+
+			*it = 9;
+			++it;
+
+			assert(it.position == 2);
+			assert(mapped[0] == 5 && mapped[1] == 9 && mapped[2] == 24 && mapped[3] == 28 && mapped[4] == 0);
+
+			*it = 82;
+			++it;
+
+			assert(it == range.end());
+			assert(it.position == 3);
+			assert(mapped[0] == 5 && mapped[1] == 9 && mapped[2] == 82 && mapped[3] == 28 && mapped[4] == 0);
+		}
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(2);
+
+		assert(mbuf.buffer_head == 0);
+		assert(range.range_head == 3);
+		assert(range.range_length == 2);
+
+		{
+			auto it = range.begin();
+
+			assert(it.position == 0);
+
+			*it = 51;
+			++it;
+
+			assert(it.position == 1);
+			assert(mapped[0] == 5 && mapped[1] == 9 && mapped[2] == 82 && mapped[3] == 51 && mapped[4] == 0);
+
+			*it = 106;
+			++it;
+
+			assert(it.position == 2);
+			assert(it == range.end());
+			assert(mapped[0] == 5 && mapped[1] == 9 && mapped[2] == 82 && mapped[3] == 51 && mapped[4] == 106);
+		}
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(5);
+
+		assert(mbuf.buffer_head == 0);
+		assert(range.range_head == 0);
+		assert(range.range_length == 5);
+
+		{
+			auto it = range.begin() + 2;
+
+			assert(it.position == 2);
+
+			*it = 98;
+			it = it--;
+
+			assert(it.position == 2);
+			assert(mapped[0] == 5 && mapped[1] == 9 && mapped[2] == 98 && mapped[3] == 51 && mapped[4] == 106);
+
+			*it = 44;
+			it = (--it) - 1;
+
+			assert(it.position == 0);
+			assert(mapped[0] == 5 && mapped[1] == 9 && mapped[2] == 44 && mapped[3] == 51 && mapped[4] == 106);
+
+			*it = 1;
+			it = it + 4;
+
+			assert(it.position == 4);
+			assert(mapped[0] == 1 && mapped[1] == 9 && mapped[2] == 44 && mapped[3] == 51 && mapped[4] == 106);
+
+			*it = 12;
+			it++;
+
+			assert(it.position == 5);
+			assert(it == range.end());
+			assert(mapped[0] == 1 && mapped[1] == 9 && mapped[2] == 44 && mapped[3] == 51 && mapped[4] == 12);
+		}
+	}
+}
+
+template <typename T> void test_mapped_buffer_contiguous_range_subscript_operator()
+{
+	T mapped[5] = { 0, 0, 0, 0, 0 };
+	win::MappedRingBuffer<T> mbuf(mapped, 5);
+
+	assert(mbuf.buffer == mapped);
+	assert(mbuf.buffer_length == 5);
+	assert(mbuf.buffer_head == 0);
+
+	{
+		auto range = mbuf.reserve_contiguous(5);
+
+		assert(mbuf.buffer_head == 0);
+		assert(range.range_head == 0);
+		assert(range.range_length == 5);
+
+		range[2] = 63;
+
+		assert(mapped[0] == 0 && mapped[1] == 0 && mapped[2] == 63 && mapped[3] == 0 && mapped[4] == 0);
+
+		range[4] = 8;
+
+		assert(mapped[0] == 0 && mapped[1] == 0 && mapped[2] == 63 && mapped[3] == 0 && mapped[4] == 8);
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(3);
+
+		assert(mbuf.buffer_head == 3);
+		assert(range.range_head == 0);
+		assert(range.range_length == 3);
+
+		range[0] = 65;
+
+		assert(mapped[0] == 65 && mapped[1] == 0 && mapped[2] == 63 && mapped[3] == 0 && mapped[4] == 8);
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(3);
+
+		assert(mbuf.buffer_head == 3);
+		assert(range.range_head == 0);
+		assert(range.range_length == 3);
+
+		range[1] = 89;
+
+		assert(mapped[0] == 65 && mapped[1] == 89 && mapped[2] == 63 && mapped[3] == 0 && mapped[4] == 8);
+	}
+
+	{
+		auto range = mbuf.reserve_contiguous(2);
+
+		assert(mbuf.buffer_head == 0);
+		assert(range.range_head == 3);
+		assert(range.range_length == 2);
+
+		range[0] = 13;
+		range[1] = 19;
+
+		assert(mapped[0] == 65 && mapped[1] == 89 && mapped[2] == 63 && mapped[3] == 13 && mapped[4] == 19);
+	}
+}
+
 int main()
 {
-	test_mapped_buffer<unsigned char>();
-	test_mapped_buffer<int>();
-	test_mapped_buffer<float>();
-	test_mapped_buffer<double>();
+	// wrap tests
+
+	test_mapped_buffer_reserve<unsigned char>();
+	test_mapped_buffer_reserve<int>();
+	test_mapped_buffer_reserve<float>();
+	test_mapped_buffer_reserve<double>();
 
 	test_mapped_buffer_range_write<unsigned char>();
 	test_mapped_buffer_range_write<int>();
 	test_mapped_buffer_range_write<float>();
 	test_mapped_buffer_range_write<double>();
 
-	test_iterator<unsigned char>();
-	test_iterator<int>();
-	test_iterator<float>();
-	test_iterator<double>();
+	test_mapped_buffer_range_iterator<unsigned char>();
+	test_mapped_buffer_range_iterator<int>();
+	test_mapped_buffer_range_iterator<float>();
+	test_mapped_buffer_range_iterator<double>();
 
-	test_subscript_operator<unsigned char>();
-	test_subscript_operator<int>();
-	test_subscript_operator<float>();
-	test_subscript_operator<double>();
+	test_mapped_buffer_range_subscript_operator<unsigned char>();
+	test_mapped_buffer_range_subscript_operator<int>();
+	test_mapped_buffer_range_subscript_operator<float>();
+	test_mapped_buffer_range_subscript_operator<double>();
+
+	// contiguous tests
+
+	test_mapped_buffer_contiguous_reserve<unsigned char>();
+	test_mapped_buffer_contiguous_reserve<int>();
+	test_mapped_buffer_contiguous_reserve<float>();
+	test_mapped_buffer_contiguous_reserve<double>();
+
+	test_mapped_buffer_range_contiguous_write<unsigned char>();
+	test_mapped_buffer_range_contiguous_write<int>();
+	test_mapped_buffer_range_contiguous_write<float>();
+	test_mapped_buffer_range_contiguous_write<double>();
+
+	test_mapped_buffer_contiguous_range_iterator<unsigned char>();
+	test_mapped_buffer_contiguous_range_iterator<int>();
+	test_mapped_buffer_contiguous_range_iterator<float>();
+	test_mapped_buffer_contiguous_range_iterator<double>();
+
+	test_mapped_buffer_contiguous_range_subscript_operator<unsigned char>();
+	test_mapped_buffer_contiguous_range_subscript_operator<int>();
+	test_mapped_buffer_contiguous_range_subscript_operator<float>();
+	test_mapped_buffer_contiguous_range_subscript_operator<double>();
 
 	fprintf(stderr, "%d asserts passed\n", successfull);
 }
--- win/include/win/MappedRingBuffer.hpp
+++ win/include/win/MappedRingBuffer.hpp
@@ -59,27 +59,41 @@ private:
 	MappedRingBufferRangeSlice slices[2];
 };
 
-template <typename T> class MappedRingBufferRangeIterator
+template <typename T> class MappedRingBufferRangeBase
+{
+public:
+	MappedRingBufferRangeBase(int head, int length, MappedRingBuffer<T> &parent)
+		: range_head(head)
+		, range_length(length)
+		, parent(parent)
+	{}
+
+	const int range_head;
+	const int range_length;
+	MappedRingBuffer<T> &parent;
+};
+
+template <typename T, bool contiguous = false> class MappedRingBufferRangeIterator
 {
 	static_assert(std::is_trivially_copyable<T>::value, "T must be trivially copyable");
 
 public:
-	MappedRingBufferRangeIterator(MappedRingBufferRange<T> *parent, int position)
+	MappedRingBufferRangeIterator(MappedRingBufferRangeBase<T> *parent, int position)
 		: parent(parent)
 		, position(position)
 	{}
 
-	MappedRingBufferRangeIterator<T> operator+(int i) const { return MappedRingBufferRangeIterator<T>(parent, position + i); }
-	MappedRingBufferRangeIterator<T> operator-(int i) const { return MappedRingBufferRangeIterator<T>(parent, position - i); }
+	MappedRingBufferRangeIterator<T, contiguous> operator+(int i) const { return MappedRingBufferRangeIterator<T, contiguous>(parent, position + i); }
+	MappedRingBufferRangeIterator<T, contiguous> operator-(int i) const { return MappedRingBufferRangeIterator<T, contiguous>(parent, position - i); }
 
-	MappedRingBufferRangeIterator<T> operator++() { ++position; return MappedRingBufferRangeIterator<T>(parent, position); }
-	MappedRingBufferRangeIterator<T> operator++(int) { ++position; return MappedRingBufferRangeIterator<T>(parent, position - 1); }
+	MappedRingBufferRangeIterator<T, contiguous> operator++() { ++position; return MappedRingBufferRangeIterator<T, contiguous>(parent, position); }
+	MappedRingBufferRangeIterator<T, contiguous> operator++(int) { ++position; return MappedRingBufferRangeIterator<T, contiguous>(parent, position - 1); }
 
-	MappedRingBufferRangeIterator<T> operator--() { --position; return MappedRingBufferRangeIterator<T>(parent, position); }
-	MappedRingBufferRangeIterator<T> operator--(int) { --position; return MappedRingBufferRangeIterator<T>(parent, position + 1); }
+	MappedRingBufferRangeIterator<T, contiguous> operator--() { --position; return MappedRingBufferRangeIterator<T, contiguous>(parent, position); }
+	MappedRingBufferRangeIterator<T, contiguous> operator--(int) { --position; return MappedRingBufferRangeIterator<T, contiguous>(parent, position + 1); }
 
-	bool operator==(const MappedRingBufferRangeIterator<T> &rhs) const { return parent == rhs.parent && position == rhs.position; }
-	bool operator!=(const MappedRingBufferRangeIterator<T> &rhs) const { return parent != rhs.parent || position != rhs.position; }
+	bool operator==(const MappedRingBufferRangeIterator<T, contiguous> &rhs) const { return parent == rhs.parent && position == rhs.position; }
+	bool operator!=(const MappedRingBufferRangeIterator<T, contiguous> &rhs) const { return parent != rhs.parent || position != rhs.position; }
 
 	T &operator*()
 	{
@@ -88,15 +102,67 @@ public:
 			win::bug("MappedRingBufferRangeIterator: Attempt to access invalid range index " + std::to_string(position) + " when range length is " + std::to_string(parent->range_length) + ".");
 #endif
 
-		return parent->parent.buffer[(parent->range_head + position) % parent->parent.buffer_length];
+		if constexpr (contiguous)
+			return parent->parent.buffer[(parent->range_head + position)];
+		else
+			return parent->parent.buffer[(parent->range_head + position) % parent->parent.buffer_length];
 	}
 
 private:
 	int position;
-	MappedRingBufferRange<T> *parent;
+	MappedRingBufferRangeBase<T> *parent;
 };
 
-template <typename T> class MappedRingBufferRange
+template <typename T> class MappedRingBufferContiguousRange : protected MappedRingBufferRangeBase<T>
+{
+	static_assert(std::is_trivially_copyable<T>::value, "T must be trivially copyable");
+	friend class MappedRingBufferRangeIterator<T>;
+	friend class MappedRingBufferRangeSliceIterable<T>;
+
+public:
+	MappedRingBufferContiguousRange(int head, int length, MappedRingBuffer<T> &parent)
+		: MappedRingBufferRangeBase<T>(head, length, parent)
+	{}
+
+	MappedRingBufferRangeIterator<T, true> begin() { return MappedRingBufferRangeIterator<T, true>(this, 0); }
+	MappedRingBufferRangeIterator<T, true> end() { return MappedRingBufferRangeIterator<T, true>(this, this->range_length); }
+
+	int head() const { return this->range_head; }
+	int length() const { return this->range_length; }
+
+	T &operator[](int index)
+	{
+#ifndef NDEBUG
+		if (index < 0 || index >= this->range_length)
+			win::bug("MappedBufferRange: index is out of bounds. Range length is " + std::to_string(this->range_length));
+#endif
+
+		const int real_index = this->range_head + index;
+		return this->parent.buffer[real_index];
+	}
+
+	int write(const T *const src, const int len) { return write(0, src, len); }
+
+	int write(const int offset, const T *const src, const int len)
+	{
+#ifndef NDEBUG
+		if (len + offset > this->range_length)
+			win::bug("MappedRingBufferContiguousRange: write too long. Requested write length was " + std::to_string(len) + " (from offset " + std::to_string(offset) + ") while the range length is only " + std::to_string(this->range_length) + ".");
+#endif
+
+		const int adjusted_head = this->range_head + offset;
+
+		const int available = this->parent.buffer_length - adjusted_head;
+		const int put = std::min(available, len);
+		const int put_bytes = put * sizeof(T);
+
+		memcpy(this->parent.buffer + adjusted_head, src, put_bytes);
+
+		return put;
+	}
+};
+
+template <typename T> class MappedRingBufferRange : protected MappedRingBufferRangeBase<T>
 {
 	static_assert(std::is_trivially_copyable<T>::value, "T must be trivially copyable");
 	friend class MappedRingBufferRangeIterator<T>;
@@ -104,77 +170,68 @@ template <typename T> class MappedRingBufferRange
 
 public:
 	MappedRingBufferRange(int head, int length, MappedRingBuffer<T> &parent)
-		: range_head(head)
-		, range_length(length)
-		, parent(parent)
+		: MappedRingBufferRangeBase<T>(head, length, parent)
 	{}
 
 	MappedRingBufferRangeIterator<T> begin() { return MappedRingBufferRangeIterator<T>(this, 0); }
-	MappedRingBufferRangeIterator<T> end() { return MappedRingBufferRangeIterator<T>(this, range_length); }
+	MappedRingBufferRangeIterator<T> end() { return MappedRingBufferRangeIterator<T>(this, this->range_length); }
 
 	MappedRingBufferRangeSliceIterable<T> slices() const { return MappedRingBufferRangeSliceIterable<T>(*this); }
 
-	int head() const { return range_head; }
-	int length() const { return range_length; }
+	int head() const { return this->range_head; }
+	int length() const { return this->range_length; }
 
 	T &operator[](int index)
 	{
 #ifndef NDEBUG
-		if (index < 0 || index >= range_length)
-			win::bug("MappedBufferRange: index is out of bounds. Range length is " + std::to_string(range_length));
+		if (index < 0 || index >= this->range_length)
+			win::bug("MappedRingBufferRange: index is out of bounds. Range length is " + std::to_string(this->range_length));
 #endif
 
-		const int real_index = (range_head + index) % parent.buffer_length;
-		return parent.buffer[real_index];
+		const int real_index = (this->range_head + index) % this->parent.buffer_length;
+		return this->parent.buffer[real_index];
 	}
 
-	int write(const T *const src, const int len)
-	{
-		return write(0, src, len);
-	}
+	int write(const T *const src, const int len) { return write(0, src, len); }
 
 	int write(const int offset, const T *const src, const int len)
 	{
 #ifndef NDEBUG
-		if (len > range_length)
-			win::bug("MappedBufferRange: write too long. Requested write length was " + std::to_string(len) + " while the range length is only " + std::to_string(range_length) + ".");
+		if (len + offset > this->range_length)
+			win::bug("MappedRingBufferRange: write too long. Requested write length was " + std::to_string(len) + " (from offset " + std::to_string(offset) + ") while the range length is only " + std::to_string(this->range_length) + ".");
 #endif
 
-		const int adjusted_head = (range_head + offset) % parent.buffer_length;
+		const int adjusted_head = (this->range_head + offset) % this->parent.buffer_length;
 
-		const int available = parent.buffer_length - adjusted_head;
+		const int available = this->parent.buffer_length - adjusted_head;
 		const int put = std::min(available, len);
 
-		const int available2 = parent.buffer_length - put;
+		const int available2 = this->parent.buffer_length - put;
 		const int put2 = std::min(available2, len - put);
 
 		const int put_bytes = put * sizeof(T);
 		const int put2_bytes = put2 * sizeof(T);
 
-		memcpy(parent.buffer + adjusted_head, src, put_bytes);
-		memcpy(parent.buffer, src + put, put2_bytes);
+		memcpy(this->parent.buffer + adjusted_head, src, put_bytes);
+		memcpy(this->parent.buffer, src + put, put2_bytes);
 
 		return put + put2;
 	}
-
-private:
-	const int range_head;
-	const int range_length;
-
-	MappedRingBuffer<T> &parent;
 };
 
 template <typename T> class MappedRingBuffer
 {
 	WIN_NO_COPY_MOVE(MappedRingBuffer);
+
 	static_assert(std::is_trivially_copyable<T>::value, "T must be trivially copyable");
 	friend class MappedRingBufferRange<T>;
+	friend class MappedRingBufferContiguousRange<T>;
 	friend class MappedRingBufferRangeIterator<T>;
 
 public:
 	MappedRingBuffer(void *mapbuf, int len_elelements)
 		: buffer_head(0)
-		, buffer(reinterpret_cast<T*>(mapbuf))
+		, buffer(reinterpret_cast<T *>(mapbuf))
 		, buffer_length(len_elelements)
 	{}
 
@@ -191,6 +248,25 @@ public:
 		MappedRingBufferRange<T> range(buffer_head, len, *this);
 
 		buffer_head = (buffer_head + len) % buffer_length;
+
+		return range;
+	}
+
+	MappedRingBufferContiguousRange<T> reserve_contiguous(int len)
+	{
+#ifndef NDEBUG
+		if (len > buffer_length)
+			win::bug("MappedRingBuffer: contiguous reservation too long. Requested length was " + std::to_string(len) + " while the total buffer length is only " + std::to_string(buffer_length) + ".");
+#endif
+
+		int start;
+		if (buffer_length - buffer_head < len)
+			start = 0;
+		else
+			start = buffer_head;
+
+		MappedRingBufferContiguousRange range(start, len, *this);
+		buffer_head = (start + len) % buffer_length;
 
 		return range;
 	}
